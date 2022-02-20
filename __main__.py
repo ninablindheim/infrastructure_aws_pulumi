d5f64e9baad559fbf2240113c564231d7e15a9b9
Nina Vinding Marstrander <72516788+ninamarstrander@users.noreply.github.com>
2022-02-20 20:52:04 +0100

    Test pipeline

--- __main__.py
+++ __main__.py
@@ -1,31 +1,29 @@
 """An AWS Python Pulumi program"""
 
-import pulumi
 from pulumi_aws import s3
 
 import pulumi
-from pulumi_aws import ec2
 
 # Create an AWS resource (S3 Bucket)
 # Set website property on bucket to make it able to serve the index.html File
 # as a static website.
 bucket = s3.Bucket('my-bucket',
-                   website=s3.BucketWebsiteArgs(
-                       index_document="index.html",
-                   ))
+                   # website=s3.BucketWebsiteArgs(
+                   #     index_document="index.html",
+                   )
 
-# FileAsset class will assign the content of the file to a new BucketOject
-bucketObject = s3.BucketObject(
-    'index.html',
-    # public-read so that it can be accessed anonymously over the Internet
-    acl='public-read',
-    # content type so that it is served as HTML
-    content_type='text/html',
-    bucket=bucket.id,
-    source=pulumi.FileAsset('index.html')
-)
+# # FileAsset class will assign the content of the file to a new BucketOject
+# bucketObject = s3.BucketObject(
+#     'index.html',
+#     # public-read so that it can be accessed anonymously over the Internet
+#     acl='public-read',
+#     # content type so that it is served as HTML
+#     content_type='text/html',
+#     bucket=bucket.id,
+#     source=pulumi.FileAsset('index.html')
+# )
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
-# export the resulting bucket's endpoint URL so you can easily access it
-pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
+# # export the resulting bucket's endpoint URL so you can easily access it
+# pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
