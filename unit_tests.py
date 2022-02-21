1aa8b9742bb053241a0dde7d56b4e57b427133db
Nina Vinding Marstrander <72516788+ninamarstrander@users.noreply.github.com>
2022-02-21 09:20:09 +0100

    Add unit test for bucket resource name

--- unit_tests.py
+++ unit_tests.py
@@ -26,3 +26,15 @@ class TestingWithMocks(unittest.TestCase):
             assert ..., 'Template error message.'
 
         return pulumi.Output.all(infra).apply(check_template)
+
+    # Test that the bucket URN matches the resource name of the bucket.
+    @pulumi.runtime.test
+    def test_bucket_urn_correctness(self) -> pulumi.Output:
+        bucket = infra.create_bucket()
+
+        def check_bucket_urn(args: list) -> None:
+            urn, = args
+            bucket_name = urn.split('::')[-1]
+            assert bucket_name == infra.BUCKET_NAME
+
+        return pulumi.Output.all(bucket.urn).apply(check_bucket_urn)
